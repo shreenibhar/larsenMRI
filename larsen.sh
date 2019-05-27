3a56aa654c291e72efc57ab6eae3f0da3f49f014
Shreenias <vshreenivasbharadwaj@gmail.com>
2019-05-27 20:19:59 +0000

    larsen.sh param fixed

--- larsen.sh
+++ larsen.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-threshnii=$1
-numModels=$2
-numStreams=$3
-l1=$4
-l2=$5
-G=$6
-maxVars=$7
-maxSteps=$8
-if [ ${#threshnii} -eq 0 -o ${#numModels} -eq 0 -o ${#numStreams} -eq 0 -o ${#l1} -eq 0 -o ${#l2} -eq 0 -o ${#G} -eq 0 -o ${#maxVars} -eq 0 -o ${#maxSteps} -eq 0 ]
+numModels=$1
+numStreams=$2
+l1=$3
+l2=$4
+G=$5
+maxVars=$6
+maxSteps=$7
+if [ ${#numModels} -eq 0 -o ${#numStreams} -eq 0 -o ${#l1} -eq 0 -o ${#l2} -eq 0 -o ${#G} -eq 0 -o ${#maxVars} -eq 0 -o ${#maxSteps} -eq 0 ]
 then
-	echo "Input consists of threshnii numModels numStreams l1 l2 G maxVars maxSteps"
+	echo "Input consists of numModels numStreams l1 l2 G maxVars maxSteps"
 	echo "Input 0 to use defaults"
 	exit
 fi
